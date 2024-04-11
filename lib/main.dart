4f2fbe1d744c0957838c6e90f87d48d676a2e301
JayceShin <chickang9@gmail.com>
2024-04-11 22:49:02 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,153 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:toonflix/widgets/button.dart';
-import 'package:toonflix/widgets/currency_card.dart';
 
 void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  List<int> numbers = [];
+
+  void onClicked() {
+    setState(() {
+      numbers.add(numbers.length);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: const Color(0xFF181818),
-          body: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(
-                    height: 80,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          const Text(
-                            'Hey, Kangsigi',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 28,
-                              fontWeight: FontWeight.w800,
-                            ),
-                          ),
-                          Text(
-                            'Welcome back',
-                            style: TextStyle(
-                              color: Colors.white.withOpacity(0.8),
-                              fontSize: 20,
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  const Text(
-                    "Total Balance",
-                    style: TextStyle(
-                      fontSize: 22,
-                      color: Colors.white,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  const Text(
-                    "\$ 5 194 482",
-                    style: TextStyle(
-                      wordSpacing: 0.01,
-                      fontSize: 44,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.white,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Button(
-                          text: 'Transfer',
-                          bgColor: Color(0xFFF2B33A),
-                          textColor: Colors.black,
-                          circular: 45,
-                          btVertical: 20,
-                          btHorizon: 50),
-                      Button(
-                          text: 'Request',
-                          bgColor: Color(0xFF1F2123),
-                          textColor: Colors.white,
-                          circular: 45,
-                          btVertical: 20,
-                          btHorizon: 50),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 40,
-                  ),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
-                        'Wallets',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 36,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                      Text(
-                        'view all',
-                        style: TextStyle(
-                          color: Colors.white.withOpacity(0.8),
-                          fontSize: 18,
-                        ),
-                      )
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  const CurrencyCard(
-                    currencyIcon: Icons.euro_rounded,
-                    currency: 'EURO',
-                    value: '6 428',
-                    unit: 'EUR',
-                    inverted: false,
-                    order: 0,
-                  ),
-                  const CurrencyCard(
-                    currencyIcon: Icons.currency_bitcoin,
-                    currency: 'Bitcoin',
-                    value: '4 23',
-                    unit: 'btc',
-                    inverted: true,
-                    order: 1,
-                  ),
-                  const CurrencyCard(
-                    currencyIcon: Icons.attach_money_rounded,
-                    currency: 'Dollar',
-                    value: '1 434 333',
-                    unit: 'USD',
-                    inverted: false,
-                    order: 2,
-                  ),
-                ],
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Click Count',
+                style: TextStyle(
+                  fontSize: 30,
+                )),
+            for (var n in numbers)
+              Text('$n',
+                  style: const TextStyle(
+                    fontSize: 30,
+                  )),
+            IconButton(
+              onPressed: onClicked,
+              icon: const Icon(
+                Icons.add_box_rounded,
+                size: 30,
               ),
-            ),
-          )),
+            )
+          ],
+        )),
+      ),
     );
   }
 }
